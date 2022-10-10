86d9731fa6e48ec31deb4f10163f1dc3ed3e5e6e
Jürgen Singer <jsinger@hs-harz.de>
2022-10-10 20:16:56 +0200

    wrote solution to Exercide 02c

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/media-informatics/aufgabe02c
+
+go 1.19
---
+++ serienbrief.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"text/template"
+)
+
+var (
+	recv  *string = flag.String("addr", "addr.csv", "Adressdatei")
+	brief *string = flag.String("brief", "serienbrief.tmpl", "Template-Datei")
+)
+
+type Recipient struct {
+	LastName string
+	Fach     string
+	Mf       bool
+}
+
+func main() {
+	flag.Parse()
+	recvFile, err := os.Open(*recv)
+	if err != nil {
+		log.Fatalf("could not open address file %w", err)
+	}
+	defer recvFile.Close()
+
+	csvReader := csv.NewReader(recvFile)
+	tokens, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatalf("could not read from address file %w", err)
+	}
+
+	addr := []Recipient{}
+	for _, line := range tokens {
+		if len(line) != 3 {
+			continue
+		}
+		b, err := strconv.ParseBool(line[2])
+		if err != nil {
+			log.Printf("invalid bool value %w", err)
+			b = true
+		}
+		r := Recipient{
+			LastName: line[0],
+			Fach:     line[1],
+			Mf:       b,
+		}
+		addr = append(addr, r)
+	}
+
+	content, err := os.ReadFile(*brief)
+	t := template.Must(template.New("Brief").Parse(string(content)))
+	for _, a := range addr {
+		out, err := os.Create(fmt.Sprintf("%s.txt", a.Fach))
+		if err != nil {
+			log.Printf("could not create letter %w", err)
+		}
+		defer out.Close()
+
+		err = t.Execute(out, a)
+		if err != nil {
+			log.Printf("error in template %w", err)
+		}
+	}
+}
